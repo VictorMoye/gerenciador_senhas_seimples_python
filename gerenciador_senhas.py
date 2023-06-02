fd8c007d421ed8eabb994aab11678342ec3e3705
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-06-02 15:27:00 -0300

    craiando os menus de comando

--- gerenciador_senhas.py
+++ gerenciador_senhas.py
@@ -13,4 +13,20 @@ cursor.execute('''
     passwrd TEXT NOT NULL
     );
 ''')
+def Menu():
+    print("* I: Inserir uma nova senha: ")
+    print("L: Listar serviços salvos: ")
+    print("*R: Recuperar senha:")
+    print("*S: Sair !")
 
+while(True):
+    Menu()
+    op = input("Qual opção deseja fazer: ")
+    if op not in ['I','L','R','S']:
+        print("Opção Invalida !!")
+        continue
+
+    if op == 's' or 'S':
+        break;
+ 
+conn.close()
